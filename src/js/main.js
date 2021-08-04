fedb2863fff96e2ffd688654ef935af2797dbe62
Zury01 <Zurisaday_01@hotmail.com>
2021-08-04 08:02:25 -0500

    Desktop finished, but problem with slider

--- src/js/main.js
+++ src/js/main.js
@@ -178,31 +178,15 @@ const swiper = new Swiper('.swiper-container', {
 	slidesPerView: 2,
 	spaceBetween: 90,
 	loop: true,
-	// breakpoints: {
-	// 	300: {
-	// 		slidesPerView: 1,
-	// 	},
-	// 	870: {
-	// 		slidesPerView: 2,
-	// 	},
-	// 	1400: {
-	// 		slidesPerView: 3,
-	// 	},
-	// },
-
-	// // If we need pagination
-	// pagination: {
-	//   el: '.swiper-pagination',
-	// },
-
-	// // Navigation arrows
-	// navigation: {
-	//   nextEl: '.swiper-button-next',
-	//   prevEl: '.swiper-button-prev',
-	// },
-
-	// // And if we need scrollbar
-	// scrollbar: {
-	//   el: '.swiper-scrollbar',
-	// },
+	breakpoints: {
+		// 	300: {
+		// 		slidesPerView: 1,
+		// 	},
+		// 	870: {
+		// 		slidesPerView: 2,
+		// 	},
+		1400: {
+			slidesPerView: 3,
+		},
+	},
 });
